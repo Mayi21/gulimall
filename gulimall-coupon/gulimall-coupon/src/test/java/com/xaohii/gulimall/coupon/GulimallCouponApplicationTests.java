469afed597d0b092d3d274ecb06dc886d1bd1e25
Mayi21 <May_i21@163.com>
2022-02-22 21:00:36 +0800

    init

---
+++ gulimall-coupon/gulimall-coupon/src/test/java/com/xaohii/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xaohii.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
